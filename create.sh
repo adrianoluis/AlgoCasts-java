17e4fbfc6b38cf1a83242123dbb198b090d4467c
Adriano Rocha <driflash@gmail.com>
2019-08-30 13:51:00 -0700

    [Chore] Add project skeleton
    Add project skeleton including maven-wrapper, gitignore, editorconfig
    and pom.xml definition file.

---
+++ create.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ "$#" -lt 1 ]
+then
+  echo "Usage: $0 <testpackage>" >&2
+  exit 1
+fi
+
+mkdir -p "src/main/java/net/adrianoluis/study/$1" "src/test/java/net/adrianoluis/study/$1" && \
+  echo "package net.adrianoluis.study.$1;\n\npublic class Solution {\n\n\tpublic void $1() {\n\t}\n}" > \
+    "src/main/java/net/adrianoluis/study/$1/Solution.java" &&
+  echo "package net.adrianoluis.study.$1;\n\nimport org.junit.Before;\n\npublic class SolutionTest {\n\n\tprivate" \
+    "Solution solution;\n\n\t@Before\n\tpublic void setUp() {\n\t\tsolution = new Solution();\n\t}\n}" > \
+    "src/test/java/net/adrianoluis/study/$1/SolutionTest.java"
