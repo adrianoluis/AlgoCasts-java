02aba956ae2c3b663849859da325e8c0fdffe644
Adriano Rocha <driflash@gmail.com>
2019-09-05 21:27:26 -0700

    [Chore] Upgrade create.sh
    Upgrades create.sh to include at least one test method in the Test file.

--- create.sh
+++ create.sh
@@ -8,6 +8,7 @@ fi
 mkdir -p "src/main/java/net/adrianoluis/study/$1" "src/test/java/net/adrianoluis/study/$1" && \
   echo "package net.adrianoluis.study.$1;\n\npublic class Solution {\n\n\tpublic void $1() {\n\t}\n}" > \
     "src/main/java/net/adrianoluis/study/$1/Solution.java" &&
-  echo "package net.adrianoluis.study.$1;\n\nimport org.junit.Before;\n\npublic class SolutionTest {\n\n\tprivate" \
-    "Solution solution;\n\n\t@Before\n\tpublic void setUp() {\n\t\tsolution = new Solution();\n\t}\n}" > \
+  echo "package net.adrianoluis.study.$1;\n\nimport org.junit.Before;\nimport org.junit.Test;\n\npublic class " \
+    "SolutionTest {\n\n\tprivate Solution solution;\n\n\t@Before\n\tpublic void setUp() {\n\t\tsolution = new " \
+    "Solution();\n\t}\n\n\t@Test\n\tpublic void test1() throws Throwable {\n\t}\n}"> \
     "src/test/java/net/adrianoluis/study/$1/SolutionTest.java"
